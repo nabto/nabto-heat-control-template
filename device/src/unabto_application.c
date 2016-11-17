a17a92ff564d02577664d199edd473634b5a9561
Ulrik Gammelby <ug@nabto.com>
2016-11-17 13:56:07 +0000

    add stub getpublic info function

--- device/src/unabto_application.c
+++ device/src/unabto_application.c
@@ -23,65 +23,91 @@ uint32_t setState(uint32_t state) {
     return state;
 }
 
+int write_string(buffer_write_t* write_buffer, const char* string) {
+    return unabto_query_write_uint8_list(write_buffer, (uint8_t *)string, strlen(string));
+}
+
 application_event_result application_event(application_request* request, buffer_read_t* read_buffer, buffer_write_t* write_buffer) {
 
     NABTO_LOG_INFO(("Nabto application_event: %u", request->queryId));
 
     switch(request->queryId) {
         // nabto_heatpump_get_state.json
-        case 10: {
-            if (!buffer_write_uint32(write_buffer, gstate)) return AER_REQ_RSP_TOO_LARGE;
+    case 10: {
+        if (!buffer_write_uint32(write_buffer, gstate)) return AER_REQ_RSP_TOO_LARGE;
 
-            return AER_REQ_RESPONSE_READY;
-        }
+        return AER_REQ_RESPONSE_READY;
+    }
         // nabto_heatpump_set_state.json
-        case 20: {
-            uint32_t state;
+    case 20: {
+        uint32_t state;
 
-            if (!buffer_read_uint32(read_buffer, &state)) return AER_REQ_TOO_SMALL;
+        if (!buffer_read_uint32(read_buffer, &state)) return AER_REQ_TOO_SMALL;
 
-            state = setState(state);
+        state = setState(state);
 
-            if (!buffer_write_uint32(write_buffer, state)) return AER_REQ_RSP_TOO_LARGE;
+        if (!buffer_write_uint32(write_buffer, state)) return AER_REQ_RSP_TOO_LARGE;
 
-            return AER_REQ_RESPONSE_READY;
-        }
+        return AER_REQ_RESPONSE_READY;
+    }
         // nabto_heatpump_get_temp.json
-        case 30: {
-            if (!buffer_write_uint32(write_buffer, gtemp)) return AER_REQ_RSP_TOO_LARGE;
+    case 30: {
+        if (!buffer_write_uint32(write_buffer, gtemp)) return AER_REQ_RSP_TOO_LARGE;
 
-            return AER_REQ_RESPONSE_READY;
-        }
+        return AER_REQ_RESPONSE_READY;
+    }
         // nabto_heatpump_set_temp.json
-        case 40: {
-            uint32_t temp;
+    case 40: {
+        uint32_t temp;
 
-            if (!buffer_read_uint32(read_buffer, &temp)) return AER_REQ_TOO_SMALL;
+        if (!buffer_read_uint32(read_buffer, &temp)) return AER_REQ_TOO_SMALL;
 
-            gtemp = temp;
+        gtemp = temp;
 
-            if (!buffer_write_uint32(write_buffer, gtemp)) return AER_REQ_RSP_TOO_LARGE;
+        if (!buffer_write_uint32(write_buffer, gtemp)) return AER_REQ_RSP_TOO_LARGE;
 
-            return AER_REQ_RESPONSE_READY;
-        }
+        return AER_REQ_RESPONSE_READY;
+    }
         // nabto_heatpump_get_mode.json
-        case 50: {
-            if (!buffer_write_uint32(write_buffer, gmode)) return AER_REQ_RSP_TOO_LARGE;
+    case 50: {
+        if (!buffer_write_uint32(write_buffer, gmode)) return AER_REQ_RSP_TOO_LARGE;
 
-            return AER_REQ_RESPONSE_READY;
-        }
+        return AER_REQ_RESPONSE_READY;
+    }
         // nabto_heatpump_set_mode.json
-        case 60: {
-            uint32_t mode;
+    case 60: {
+        uint32_t mode;
 
-            if (!buffer_read_uint32(read_buffer, &mode)) return AER_REQ_TOO_SMALL;
+        if (!buffer_read_uint32(read_buffer, &mode)) return AER_REQ_TOO_SMALL;
 
-            gmode = mode;
+        gmode = mode;
 
-            if (!buffer_write_uint32(write_buffer, gmode)) return AER_REQ_RSP_TOO_LARGE;
+        if (!buffer_write_uint32(write_buffer, gmode)) return AER_REQ_RSP_TOO_LARGE;
 
-            return AER_REQ_RESPONSE_READY;
-        }
+        return AER_REQ_RESPONSE_READY;
+    }
+
+/*
+  <query name="getPublicDeviceInfo"  id="1000">
+    <request>
+    </request>
+    <response format="json">
+      <parameter name="deviceName" type="raw"/>
+      <parameter name="deviceType" type="raw"/>
+      <parameter name="deviceIcon" type="raw"/>
+    </response>
+  </query>
+ */
+    case 1000: {
+        const char* deviceName = "Apollovej Stuen 3";
+        const char* deviceType = "ACME 9001 Heatpump";
+        const char* deviceIcon = "img/chip-small.png";
+        if (!write_string(write_buffer, deviceName)) return AER_REQ_RSP_TOO_LARGE;
+        if (!write_string(write_buffer, deviceType)) return AER_REQ_RSP_TOO_LARGE;
+        if (!write_string(write_buffer, deviceIcon)) return AER_REQ_RSP_TOO_LARGE;
+        return AER_REQ_RESPONSE_READY;
+    }
+          
     }
     return AER_REQ_INV_QUERY_ID;
 }
